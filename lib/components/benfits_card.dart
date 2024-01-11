519058966d56b5503282677fae41782e49838e0c
Freemarn <picoarman@gmail.com>
2024-01-11 01:20:29 -0800

    tawk chhat ongoing

--- lib/components/benfits_card.dart
+++ lib/components/benfits_card.dart
@@ -5,63 +5,53 @@ class BenefitCard extends StatelessWidget {
   final String benefitText;
   final String benefitHeaderText;
   final IconData cardIcon;
-  
 
-  const BenefitCard({super.key, required this.benefitText, required this.benefitHeaderText, required this.cardIcon});
+  const BenefitCard(
+      {super.key,
+      required this.benefitText,
+      required this.benefitHeaderText,
+      required this.cardIcon});
 
   @override
   Widget build(BuildContext context) {
-    return  Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      height: 30,
-                                      width: 30,
-                                      decoration: BoxDecoration(
-                                          color: AppColors.mainColor
-                                              .withOpacity(0.7),
-                                          borderRadius:
-                                              BorderRadius.circular(5)),
-                                      child: Icon(
-                                        cardIcon,
-                                        size: 17,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                0.01),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          benefitHeaderText,
-                                          
-                                          style: const TextStyle(
-                                              height: 1,
-                                              fontWeight: FontWeight.w900,
-                                              fontSize: 13,
-                                              color: AppColors.headerTextColor),
-                                        ),
-                                        SizedBox(
-                                            height: MediaQuery.of(context)
-                                                    .size
-                                                    .height *
-                                                0.01),
-                                        Text(
-                                          benefitText,
-                                          
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w900,
-                                              fontSize: 10,
-                                              color:
-                                                  Colors.grey.withOpacity(0.7)),
-                                        ),
-                                      ],
-                                    )
-                                  ],)
-                              ;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+              color: AppColors.mainColor.withOpacity(0.7),
+              borderRadius: BorderRadius.circular(5)),
+          child: Icon(
+            cardIcon,
+            size: 17,
+            color: Colors.white,
+          ),
+        ),
+        SizedBox(width: MediaQuery.of(context).size.width * 0.01),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              benefitHeaderText,
+              style: const TextStyle(
+                  height: 1,
+                  fontWeight: FontWeight.w900,
+                  fontSize: 13,
+                  color: AppColors.headerTextColor),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+            Text(
+              benefitText,
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 10,
+                  color: Colors.grey.withOpacity(0.7)),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
